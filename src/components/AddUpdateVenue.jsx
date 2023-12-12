609b59d8e40615765f3dcc98c4abcf49032d780d
Ebrar Kadir Çetin <cetinebrarkadir@gmail.com>
2023-12-12 20:29:20 +0300

    0.3

--- src/components/AddUpdateVenue.jsx
+++ src/components/AddUpdateVenue.jsx
@@ -3,7 +3,6 @@ import Header from "./Header";
 
 
 const AddUpdateVenue = () => {
-  // State tanımlamaları
   const [mekanAdi, setMekanAdi] = useState('');
   const [mekanAdresi, setMekanAdresi] = useState('');
   const [imkanlar, setImkanlar] = useState('');
@@ -18,10 +17,7 @@ const AddUpdateVenue = () => {
   const [kapanis2, setKapanis2] = useState('');
   const [kapali2, setKapali2] = useState(false);
 
-  // Mekan ekle veya güncelle fonksiyonu
   const handleAddUpdateVenue = () => {
-    // Burada mekan bilgilerini kullanarak API'ye istek gönderme veya başka bir işlem yapabilirsiniz
-    // Örneğin: axios.post('/api/mekanlar', { mekanAdi, mekanAdresi, ... })
   };
 
   return (
@@ -29,48 +25,63 @@ const AddUpdateVenue = () => {
       <Header headerText="Yeni Mekan Ekle"/>
       <form>
         
-        <label>Mekan Adı:</label>
-        <input type="text" value={mekanAdi} onChange={(e) => setMekanAdi(e.target.value)} />
+          <table>
+        <tr>
+          <td><label for="mekanAdi">Mekan Adı:</label></td>
+          <td><input type="text" id="mekanAdi" value={mekanAdi} onChange={(e) => setMekanAdi(e.target.value)} /></td>
+        </tr>
+        <tr>
+          <td><label for="mekanAdresi">Mekan Adresi:</label></td>
+          <td><input type="text" id="mekanAdresi" value={mekanAdresi} onChange={(e) => setMekanAdresi(e.target.value)} /></td>
+        </tr>
+        <tr>
+          <td><label for="imkanlar">İmkanlar:</label></td>
+          <td><input type="text" id="imkanlar" value={imkanlar} onChange={(e) => setImkanlar(e.target.value)} /></td>
+        </tr>
+        <tr>
+          <td><label for="enlem">Enlem:</label></td>
+          <td><input type="text" id="enlem" value={enlem} onChange={(e) => setEnlem(e.target.value)} /></td>
+        </tr>
+        <tr>
+          <td><label for="boylam">Boylam:</label></td>
+          <td><input type="text" id="boylam" value={boylam} onChange={(e) => setBoylam(e.target.value)} /></td>
+        </tr>
+        <tr>
+          <td><label for="gun1">Günler-1:</label></td>
+          <td><input type="text" id="gun1" value={gun1} onChange={(e) => setGun1(e.target.value)} /></td>
+        </tr>
+        <tr>
+          <td><label for="acilis1">Açılış-1:</label></td>
+          <td><input type="text" id="acilis1" value={acilis1} onChange={(e) => setAcilis1(e.target.value)} /></td>
+        </tr>
+        <tr>
+          <td><label for="kapanis1">Kapanış-1:</label></td>
+          <td><input type="text" id="kapanis1" value={kapanis1} onChange={(e) => setKapanis1(e.target.value)} /></td>
+        </tr>
+        <tr>
+          <td><label for="kapali1">Kapalı-1:</label></td>
+          <td><input type="checkbox" checked={kapali1} onChange={() => setKapali1(!kapali1)} /></td>
+        </tr>
+        <tr>
+          <td><label for="gun2">Günler-2:</label></td>
+          <td><input type="text" id="gun2" value={gun2} onChange={(e) => setGun2(e.target.value)} /></td>
+        </tr>
+        <tr>
+          <td><label for="acilis2">Açılış-2:</label></td>
+          <td><input type="text" id="acilis2" value={acilis2} onChange={(e) => setAcilis2(e.target.value)} /></td>
+        </tr>
+        <tr>
+          <td><label for="kapanis2">Kapanış-2:</label></td>
+          <td><input type="text" id="kapanis2" value={kapanis2} onChange={(e) => setKapanis2(e.target.value)} /></td>
+        </tr>
+        <tr>
+          <td><label for="kapali2">Kapalı-2:</label></td>
+          <td><input type="checkbox" checked={kapali2} onChange={() => setKapali2(!kapali2)} /></td>
+        </tr>
+      </table>
 
-        <label>Mekan Adresi:</label>
-        <input type="text" value={mekanAdresi} onChange={(e) => setMekanAdresi(e.target.value)} />
-
-        <label>İmkanlar:</label>
-        <input type="text" value={imkanlar} onChange={(e) => setImkanlar(e.target.value)} />
-
-        <label>Enlem:</label>
-        <input type="text" value={enlem} onChange={(e) => setEnlem(e.target.value)} />
-
-        <label>Boylam:</label>
-        <input type="text" value={boylam} onChange={(e) => setBoylam(e.target.value)} />
-
-        <label>Günler-1:</label>
-        <input type="text" value={gun1} onChange={(e) => setGun1(e.target.value)} />
-
-        <label>Açılış-1:</label>
-        <input type="text" value={acilis1} onChange={(e) => setAcilis1(e.target.value)} />
-
-        <label>Kapanış-1:</label>
-        <input type="text" value={kapanis1} onChange={(e) => setKapanis1(e.target.value)} />
-
-        <label>Kapalı-1:</label>
-        <input type="checkbox" checked={kapali1} onChange={() => setKapali1(!kapali1)} />
-
-        <label>Günler-2:</label>
-        <input type="text" value={gun2} onChange={(e) => setGun2(e.target.value)} />
-
-        <label>Açılış-2:</label>
-        <input type="text" value={acilis2} onChange={(e) => setAcilis2(e.target.value)} />
-
-        <label>Kapanış-2:</label>
-        <input type="text" value={kapanis2} onChange={(e) => setKapanis2(e.target.value)} />
-
-        <label>Kapalı-2:</label>
-        <input type="checkbox" checked={kapali2} onChange={() => setKapali2(!kapali2)} />
-
-        {/* Mekanı Ekle/Güncelle butonu */}
         <button type="button" onClick={handleAddUpdateVenue}>
-          Mekanı Ekle/Güncelle
+          Mekanı Ekle
         </button>
       </form>
     </>
